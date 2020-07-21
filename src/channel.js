07db4d983d52a6fae0c1d9d921839c7b19e219e7
Rob Dawson <rob@codebox.org.uk>
2020-07-21 11:42:36 +0100

    unit tests for playlist generation

--- src/channel.js
+++ src/channel.js
@@ -1,87 +1,89 @@
-const channels = {},
-    START_TIME = 1595199600; // 2020-07-20 00:00:00
+const
+    START_TIME = 1595199600, // 2020-07-20 00:00:00
+    DEFAULT_CLOCK = {
+        now() {
+            "use strict";
+            return Date.now() / 1000
+        }
+    },
+    PLAYLIST_MIN_LENGTH = 60 * 60;
 
-let clock = {
-    now() {
-        "use strict";
-        return Date.now() / 1000
-    }
-};
+module.exports.buildChannelManager = (clock = DEFAULT_CLOCK, playlistMinLengthInSeconds = PLAYLIST_MIN_LENGTH) => {
+    const channels = {};
 
-module.exports = {
-    addChannel(channelId, shows) {
-        "use strict";
+    return {
+        addChannel(channelId, shows) {
+            "use strict";
 
-        const episodeCount = shows.flatMap(show => show.files).length,
-            remainingForEachShow = {};
+            const episodeCount = shows.flatMap(show => show.files).length,
+                remainingForEachShow = {};
 
-        shows.forEach(show => {
-            remainingForEachShow[show.id] = {
-                startCount: show.files.length,
-                remaining: show.files.length
+            shows.forEach(show => {
+                remainingForEachShow[show.id] = {
+                    startCount: show.files.length,
+                    remaining: show.files.length
+                };
+            });
+            console.log(`Build '${channelId}' channel with ${episodeCount} episodes`);
+
+            const episodeList = [];
+
+            for (let i = 0; i < episodeCount; i++) {
+                const nextShowId = Object.entries(remainingForEachShow).map(kv => {
+                    return {
+                        showId: kv[0],
+                        remainingFraction: (kv[1].remaining - 1) / kv[1].startCount
+                    };
+                }).sort((o1, o2) => o2.remainingFraction - o1.remainingFraction)[0].showId;
+
+                const remainingForNextShow = remainingForEachShow[nextShowId],
+                    nextShow = shows.find(s => s.id === nextShowId),
+                    nextFile = nextShow.files[remainingForNextShow.startCount - remainingForNextShow.remaining],
+                    nextEpisode = {
+                        url: `${nextShow.urlPrefixes[0]}${nextFile.file}`,
+                        length: nextFile.length
+                    };
+                remainingForNextShow.remaining--;
+                episodeList.push(nextEpisode);
+            }
+
+            channels[channelId] = {
+                title: channelId,
+                duration: episodeList[episodeList.length - 1].offset,
+                list: episodeList
             };
-        });
-        console.log(`Build '${channelId}' channel with ${episodeCount} episodes`);
+        },
+        getPlaylist(channelId) {
+            "use strict";
+            const channel = channels[channelId],
+                playlistLength = channel.list.reduce((lengthSoFar, currentItem) => lengthSoFar + currentItem.length, 0),
+                offsetSinceStartOfPlay = (clock.now() - START_TIME) % playlistLength;
+            let i = 0, clientPlaylist = [], currentOffset = 0, currentProgrammeDuration, currentItem;
 
-        const episodeList = [];
+            let initialOffset;
+            while (true) {
+                currentItem = channel.list[i % channel.list.length];
+                if (currentOffset + currentItem.length > offsetSinceStartOfPlay) {
+                    initialOffset = offsetSinceStartOfPlay - currentOffset;
+                    break;
+                }
+                currentOffset += currentItem.length;
+                i++;
+            }
 
-        for (let i=0; i<episodeCount; i++) {
-            const nextShowId = Object.entries(remainingForEachShow).map(kv => {
-                return {
-                    showId: kv[0],
-                    remainingFraction: (kv[1].remaining - 1) / kv[1].startCount
-                };
-            }).sort((o1, o2) => o2.remainingFraction - o1.remainingFraction)[0].showId;
+            currentProgrammeDuration = -initialOffset;
+            while (currentProgrammeDuration < playlistMinLengthInSeconds) {
+                const currentItem = channel.list[i % channel.list.length];
+                clientPlaylist.push(currentItem);
+                currentProgrammeDuration += currentItem.length;
+                i++;
+            }
 
-            const remainingForNextShow = remainingForEachShow[nextShowId],
-                nextShow = shows.find(s => s.id === nextShowId),
-                nextFile = nextShow.files[remainingForNextShow.startCount - remainingForNextShow.remaining],
-                nextEpisode = {
-                    url: `${nextShow.urlPrefixes[0]}${nextFile.file}`,
-                    length: nextFile.length
-                };
-            remainingForNextShow.remaining--;
-            episodeList.push(nextEpisode);
+            return {
+                list: clientPlaylist,
+                initialOffset
+            };
         }
-
-        channels[channelId] = {
-            title: channelId,
-            duration: episodeList[episodeList.length - 1].offset,
-            list: episodeList
-        };
-    },
-    getPlaylist(channelId) {
-        "use strict";
-        const minProgrammeDuration = 60 * 60, // how long must the playlist be that we return?
-            playlist = channels[channelId],
-            playlistLength = playlist.list.reduce((lengthSoFar, currentItem) => lengthSoFar + currentItem.length, 0),
-            offsetSinceStartOfPlay = (clock.now() - START_TIME) % playlistLength;
-
-        let i = 0, clientPlaylist = [], currentOffset = 0, currentProgrammeDuration = 0, currentItem;
-
-        while (currentOffset < offsetSinceStartOfPlay) {
-            currentItem = playlist.list[i % playlist.list.length];
-            currentOffset += currentItem.length;
-            i++;
-        }
-        clientPlaylist.push(currentItem);
-        const initialOffset = offsetSinceStartOfPlay - currentOffset + currentItem.length;
-
-        while (currentProgrammeDuration < minProgrammeDuration) {
-            const currentItem = playlist.list[i % playlist.list.length];
-            clientPlaylist.push(currentItem);
-            currentProgrammeDuration += currentItem.length;
-            i++;
-        }
-
-        return {
-            list: clientPlaylist,
-            initialOffset
-        };
-    },
-    setClock(_clock) { // for unit tests
-        "use strict";
-        clock = _clock;
-    }
+    };
 };
 
