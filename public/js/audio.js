e6c66b5ae4500466409fe8a997aa304c1a352d95
Rob Dawson <rob@codebox.org.uk>
2020-07-30 12:33:08 +0100

    partial fix for ios problems

--- public/js/audio.js
+++ public/js/audio.js
@@ -11,15 +11,17 @@ const audioPlayer = (() => {
     function initAudio() {
         if (!audioInitialised) {
             audio.crossOrigin = "anonymous";
+            if (window.AudioContext) {
+                // webkitAudioContext doesn't work
+                const audioCtx = new window.AudioContext(),
+                    audioSrc = audioCtx.createMediaElementSource(audio);
+                analyser = audioCtx.createAnalyser();
+                analyser.fftSize = FFT_WINDOW_SIZE;
+                analyser.smoothingTimeConstant = SMOOTHING;
 
-            const audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
-                audioSrc = audioCtx.createMediaElementSource(audio);
-            analyser = audioCtx.createAnalyser();
-            analyser.fftSize = FFT_WINDOW_SIZE;
-            analyser.smoothingTimeConstant = SMOOTHING;
-
-            audioSrc.connect(analyser);
-            analyser.connect(audioCtx.destination);
+                audioSrc.connect(analyser);
+                analyser.connect(audioCtx.destination);
+            }
             audioInitialised = true;
         }
     }
@@ -59,9 +61,13 @@ const audioPlayer = (() => {
         },
         getData() {
             initAudio();
-            const dataArray = new Uint8Array(BUFFER_LENGTH);
-            analyser.getByteFrequencyData(dataArray);
-            return dataArray;
+            if (analyser) {
+                const dataArray = new Uint8Array(BUFFER_LENGTH);
+                analyser.getByteFrequencyData(dataArray);
+                return dataArray;
+            } else {
+                return [];
+            }
         }
     };
 })();
