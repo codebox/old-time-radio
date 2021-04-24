1339686a0f5c2c8a3d791b75a1053470f3c54b1a
Rob Dawson <rob@codebox.org.uk>
2021-04-24 17:06:46 +0100

    visualiser refactoring

--- public/js/visualiser.js
+++ public/js/visualiser.js
@@ -18,87 +18,94 @@ function buildVisualiser(dataFactory) {
         return `rgb(${v},${v},${v})`;
     }
 
-    const timelapseHistory = [], timelapseData = dataFactory.audioDataSource()
-        .withBucketCount(100)
-        .withRedistribution(1.5)
-        .withShuffling()
-        .withFiltering(5000)
-        .build();
-    function timelapse() {
-        const maxHistory = 20,
-            historyLag = 5,
-            minWidth = 0.3 * width,
-            maxWidth = 0.9 * width,
-            barWidthFraction = 0.2,
-            borderY = 50,
-            rowGap = (height - 2 * borderY) / (maxHistory - 1),
-            dataBuckets = timelapseData.get();
-
-        timelapseHistory.push(dataBuckets);
-        if (timelapseHistory.length > maxHistory * historyLag) {
-            timelapseHistory.shift();
-        }
-        clearCanvas();
-        timelapseHistory.filter((row, rowIndex) => (maxHistory * historyLag - rowIndex) % historyLag === 0).forEach((row, rowIndex) => {
-            const rowWidth = minWidth + (maxWidth - minWidth) * rowIndex / (maxHistory - 1),
-                rowScale = rowWidth / maxWidth,
-                rowXStart = (width - rowWidth) / 2,
-                rowBarAndGapWidth = rowWidth / row.length,
-                rowBarWidth = rowBarAndGapWidth * barWidthFraction,
-                rowMaxHeight = 100 * rowScale,
-                rowYBase = borderY + rowGap * rowIndex,
-                colourPart = Math.round(255 * (0.2 + 0.8 * rowIndex / (maxHistory - 1)));
-
-            ctx.fillStyle = `rgb(${colourPart}, ${colourPart}, ${colourPart})`;
-            row.forEach((value, valIndex) => {
-                const x = rowXStart + valIndex * rowBarAndGapWidth,
-                    y = rowYBase - rowMaxHeight * value,
-                    w = rowBarWidth,
-                    h = rowMaxHeight * value || 1;
-                ctx.fillRect(x, y, w, h);
-            });
-        });
-    }
-
-    const experimentalDataSource = dataFactory.audioDataSource().withBucketCount(10).withRedistribution(2).build();
-    function experimental() {
-        const xBorderWidth = 50,
-            yBorderWidth = 20,
-            maxHeight = height - 2 * yBorderWidth,
-            dataBuckets = experimentalDataSource.get(),
-            bucketCount = dataBuckets.length,
-            barSeparation = 5,
-            barWidth = (width - 2 * xBorderWidth - (bucketCount-1) * barSeparation) / bucketCount;
-
-        let xBarStart = xBorderWidth;
-        clearCanvas();
-        ctx.fillStyle = 'white';
-        dataBuckets.forEach((value, i) => {
-            const x = xBarStart,
-                y = yBorderWidth + maxHeight * (1 - value),
-                w = barWidth,
-                h = maxHeight * value;
-            ctx.fillRect(x, y, w, h);
-            xBarStart += (barSeparation + barWidth);
-        });
-    }
-
-    const phonographConfig = config.visualiser.phonograph,
-        phonographDataSource = dataFactory.audioDataSource()
-            .withBucketCount(phonographConfig.bucketCount)
-            .withRedistribution(phonographConfig.bucketSpread)
-            .withFiltering(phonographConfig.silenceThresholdMillis)
+    const timelapse = (() => {
+        const history = [], audioData = dataFactory.audioDataSource()
+            .withBucketCount(100)
+            .withRedistribution(1.5)
             .withShuffling()
+            .withFiltering(5000)
             .build();
 
-    const phonograph = (() => {
-        let startTs = Date.now(), snapshots = [], lastSnapshotTs = Date.now();
+        return () => {
+            const maxHistory = 20,
+                historyLag = 5,
+                minWidth = 0.3 * width,
+                maxWidth = 0.9 * width,
+                barWidthFraction = 0.2,
+                borderY = 50,
+                rowGap = (height - 2 * borderY) / (maxHistory - 1),
+                dataBuckets = audioData.get();
 
+            history.push(dataBuckets);
+            if (history.length > maxHistory * historyLag) {
+                history.shift();
+            }
+            clearCanvas();
+            history.filter((row, rowIndex) => (maxHistory * historyLag - rowIndex) % historyLag === 0).forEach((row, rowIndex) => {
+                const rowWidth = minWidth + (maxWidth - minWidth) * rowIndex / (maxHistory - 1),
+                    rowScale = rowWidth / maxWidth,
+                    rowXStart = (width - rowWidth) / 2,
+                    rowBarAndGapWidth = rowWidth / row.length,
+                    rowBarWidth = rowBarAndGapWidth * barWidthFraction,
+                    rowMaxHeight = 100 * rowScale,
+                    rowYBase = borderY + rowGap * rowIndex,
+                    colourPart = Math.round(255 * (0.2 + 0.8 * rowIndex / (maxHistory - 1)));
+
+                ctx.fillStyle = `rgb(${colourPart}, ${colourPart}, ${colourPart})`;
+                row.forEach((value, valIndex) => {
+                    const x = rowXStart + valIndex * rowBarAndGapWidth,
+                        y = rowYBase - rowMaxHeight * value,
+                        w = rowBarWidth,
+                        h = rowMaxHeight * value || 1;
+                    ctx.fillRect(x, y, w, h);
+                });
+            });
+        }
+    })();
+
+    const experimental = (() => {
+        const audioData = dataFactory.audioDataSource()
+            .withBucketCount(10)
+            .withRedistribution(2)
+            .build();
+
+        return () => {
+            const xBorderWidth = 50,
+                yBorderWidth = 20,
+                maxHeight = height - 2 * yBorderWidth,
+                dataBuckets = audioData.get(),
+                bucketCount = dataBuckets.length,
+                barSeparation = 5,
+                barWidth = (width - 2 * xBorderWidth - (bucketCount-1) * barSeparation) / bucketCount;
+
+            let xBarStart = xBorderWidth;
+            clearCanvas();
+            ctx.fillStyle = 'white';
+            dataBuckets.forEach((value, i) => {
+                const x = xBarStart,
+                    y = yBorderWidth + maxHeight * (1 - value),
+                    w = barWidth,
+                    h = maxHeight * value;
+                ctx.fillRect(x, y, w, h);
+                xBarStart += (barSeparation + barWidth);
+            });
+        };
+    })();
+
+    const phonograph = (() => {
         const phonographConfig = config.visualiser.phonograph,
             minRadius = phonographConfig.minRadius,
             gapTotal = phonographConfig.gapTotal,
             snapshotStartColour = phonographConfig.snapshotStartColour,
-            snapshotStopColour = phonographConfig.snapshotStopColour;
+            snapshotStopColour = phonographConfig.snapshotStopColour,
+            audioData = dataFactory.audioDataSource()
+                .withBucketCount(phonographConfig.bucketCount)
+                .withRedistribution(phonographConfig.bucketSpread)
+                .withFiltering(phonographConfig.silenceThresholdMillis)
+                .withShuffling()
+                .build();
+
+        let startTs = Date.now(), snapshots = [], lastSnapshotTs = Date.now();
 
         return () => {
             const cx = width / 2,
@@ -108,7 +115,7 @@ function buildVisualiser(dataFactory) {
 
             clearCanvas();
 
-            const dataBuckets = phonographDataSource.get();
+            const dataBuckets = audioData.get();
 
             const anglePerBucket = Math.PI * 2 / dataBuckets.length;
 
@@ -173,76 +180,80 @@ function buildVisualiser(dataFactory) {
         };
     })();
 
-
-    const oscillographDataSource = dataFactory.audioDataSource()
+    const oscillograph = (() => {
+        const audioData = dataFactory.audioDataSource()
             .withBucketCount(config.visualiser.oscillograph.bucketCount)
             .withFiltering(5000)
             .build();
-    function oscillograph() {
-        const WAVE_SPEED = config.visualiser.oscillograph.waveSpeed,
-            PADDING = width > 500 ? 50 : 25,
-            MIN_WAVE_LIGHTNESS = config.visualiser.oscillograph.minWaveLightness,
-            TWO_PI = Math.PI * 2,
-            startX = PADDING,
-            endX = width - PADDING;
 
-        const dataBuckets = oscillographDataSource.get();
+        return () => {
+            const WAVE_SPEED = config.visualiser.oscillograph.waveSpeed,
+                PADDING = width > 500 ? 50 : 25,
+                MIN_WAVE_LIGHTNESS = config.visualiser.oscillograph.minWaveLightness,
+                TWO_PI = Math.PI * 2,
+                startX = PADDING,
+                endX = width - PADDING;
 
-        clearCanvas();
-        dataBuckets.forEach((v, i) => {
-            function calcY(x) {
-                const scaledX = TWO_PI * (x - startX) / (endX - startX);
-                return (height / 2) + Math.sin(scaledX * (i + 1)) * v * height / 2;
-            }
+            const dataBuckets = audioData.get();
 
-            ctx.strokeStyle = `hsl(0,0%,${Math.floor(MIN_WAVE_LIGHTNESS + (100 - MIN_WAVE_LIGHTNESS) * (1 - v))}%)`;
-            ctx.beginPath();
-            let first = true;
-
-            for (let x = startX; x < endX; x++) {
-                const y = height - calcY(x - step * (i + 1));
-                if (first) {
-                    ctx.moveTo(x, y);
-                    first = false;
+            clearCanvas();
+            dataBuckets.forEach((v, i) => {
+                function calcY(x) {
+                    const scaledX = TWO_PI * (x - startX) / (endX - startX);
+                    return (height / 2) + Math.sin(scaledX * (i + 1)) * v * height / 2;
                 }
-                ctx.lineTo(x, y);
-            }
-            ctx.stroke();
-        });
 
-        step = (step + WAVE_SPEED);
-    }
+                ctx.strokeStyle = `hsl(0,0%,${Math.floor(MIN_WAVE_LIGHTNESS + (100 - MIN_WAVE_LIGHTNESS) * (1 - v))}%)`;
+                ctx.beginPath();
+                let first = true;
 
-    const circularDataSource = dataFactory.audioDataSource()
-        .withBucketCount(30)
-        .withRedistribution(1.5)
-        .withShuffling()
-        .withFiltering(5000)
-        .build();
+                for (let x = startX; x < endX; x++) {
+                    const y = height - calcY(x - step * (i + 1));
+                    if (first) {
+                        ctx.moveTo(x, y);
+                        first = false;
+                    }
+                    ctx.lineTo(x, y);
+                }
+                ctx.stroke();
+            });
 
-    function circular() {
-        const dataBuckets = circularDataSource.get();
+            step = (step + WAVE_SPEED);
+        }
+    })();
 
-        clearCanvas();
-        const minRadius = 100,
-            bucketCount = dataBuckets.length,
-            angleDiff = Math.PI * 2 / bucketCount,
-            cx = width / 2,
-            cy = height / 2,
-            maxRadius = 150;//Math.min(width, height) / 2;
+    const circular = (() => {
+        const audioData = dataFactory.audioDataSource()
+            .withBucketCount(30)
+            .withRedistribution(1.5)
+            .withShuffling()
+            .withFiltering(5000)
+            .build();
 
-        ctx.fillStyle = 'white';
-        dataBuckets.forEach((value, i) => {
-            const radius = minRadius + (maxRadius - minRadius) * value,
-                angle = i * angleDiff,
-                x = cx + Math.sin(angle) * radius,
-                y = cy + Math.cos(angle) * radius;
-            ctx.beginPath();
-            ctx.moveTo(x, y);
-            ctx.arc(x, y, 5, 0, 2 * Math.PI, false);
-            ctx.fill();
-        });
-    }
+        return () => {
+            const dataBuckets = audioData.get();
+
+            clearCanvas();
+            const minRadius = 100,
+                bucketCount = dataBuckets.length,
+                angleDiff = Math.PI * 2 / bucketCount,
+                cx = width / 2,
+                cy = height / 2,
+                maxRadius = 150;//Math.min(width, height) / 2;
+
+            ctx.fillStyle = 'white';
+            dataBuckets.forEach((value, i) => {
+                const radius = minRadius + (maxRadius - minRadius) * value,
+                    angle = i * angleDiff,
+                    x = cx + Math.sin(angle) * radius,
+                    y = cy + Math.cos(angle) * radius;
+                ctx.beginPath();
+                ctx.moveTo(x, y);
+                ctx.arc(x, y, 5, 0, 2 * Math.PI, false);
+                ctx.fill();
+            });
+        };
+    })();
 
     const visualiserLookup = {
         "None": () => {},
