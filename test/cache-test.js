e4f842fba37964c2a401342bf22ca3df872c96fe
Rob Dawson <rob@codebox.org.uk>
2021-05-21 18:33:34 +0100

    cache testing

--- test/cache-test.js
+++ test/cache-test.js
@@ -5,7 +5,6 @@ const cacheBuilder = require('../src/cache.js'),
 describe("cache", () => {
     let sourceIds;
     function source(id) {
-        console.log('source')
         sourceIds.push(id);
         return Promise.resolve(`result=${id}`);
     }
@@ -41,7 +40,31 @@ describe("cache", () => {
             expect(await cache.get('b')).toBe('result=b');
             expect(sourceIds).toEqual(['a', 'b']);
         });
-
     });
 
+    describe("items expire correctly", () => {
+        let cache;
+
+        beforeEach(async () => {
+            cache = cacheBuilder.buildCache("test", source, {expiryIntervalSeconds: 2});
+            await cache.init();
+        });
+
+        it("source only called once per id", async done => {
+            expect(sourceIds).toEqual([]);
+
+            expect(await cache.get('a')).toBe('result=a');
+            expect(sourceIds).toEqual(['a']);
+
+            expect(await cache.get('a')).toBe('result=a');
+            expect(sourceIds).toEqual(['a']);
+
+            setTimeout(async () => {
+                expect(await cache.get('a')).toBe('result=a');
+                expect(sourceIds).toEqual(['a', 'a']);
+                done();
+            }, 3000);
+        });
+
+    });
 });
