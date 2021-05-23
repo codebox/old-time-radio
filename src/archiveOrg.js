c5296a579edf5f9c3a8a3d5e72dfb59a4c6301fc
Rob Dawson <rob@codebox.org.uk>
2021-05-23 14:43:21 +0100

    added archive.org search function

--- src/archiveOrg.js
+++ src/archiveOrg.js
@@ -22,5 +22,22 @@ module.exports = {
     getPlaylist(id) {
         // Example url: https://archive.org/metadata/OTRR_Space_Patrol_Singles
         return webClient.get(`https://archive.org/metadata/${id}`);
+    },
+    /*
+     Example JSON (only showing data used by the application):
+     {
+        "response": {
+            "numFound": 6910,
+            "docs": [
+                {"identifier": "78_the-gipsys-warning_betsy-lane-shepherd-henry-a-coard_gbia0083657b"},
+                {"identifier": "78_what-do-you-say_hughie-barrett-yellen-ager-ash_gbia0074620a"},
+                ...
+            ]
+        }
+     }
+     */
+    search(collection, rowCount, page, fromDate, toDate) {
+        const query = encodeURIComponent(`collection:(${collection}) AND mediatype:(audio) AND date:[${fromDate} TO ${toDate}]`);
+        return webClient.get(`https://archive.org/advancedsearch.php?q=${query}&fl[]=identifier&rows=${rowCount}&page=${page}&output=json`);
     }
 };
